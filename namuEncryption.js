1a1fc4e2c7063045ac6303d2caf27034a7f5b7e8
ThomasSAZERAT <thomas.sazerat@etu.unilim.fr>
2023-02-08 14:44:30 +0100

    Now all is async, use await for bcrypt function call

--- namuEncryption.js
+++ namuEncryption.js
@@ -5,15 +5,15 @@ const bcrypt = require("bcrypt");
 /**
  * @param {String} plainText
  */
-exports.encrypt = function encrypt(plainText) {
+exports.encrypt = async function encrypt(plainText) {
     const salt = 10;
-    return bcrypt.hash(plainText, salt);
+    return await bcrypt.hash(plainText, salt);
 }
 
 /**
  * @param {String} plainText
  * @param {String} encryptedText
  */
-exports.compare = function compare(plainText, encryptedText) {
-    return bcrypt.compare(plainText, encryptedText);
+exports.compare = async function compare(plainText, encryptedText) {
+    return await bcrypt.compare(plainText, encryptedText);
 }
